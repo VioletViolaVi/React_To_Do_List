67f13be8278952f3d3d6708f9f77afeeaa10aa1b
VioletViola <vivian.v18723@gmail.com>
2021-05-21 15:25:39 +0100

    Add to do list items.

---
+++ to_do_list/src/myComponents/Body/ToDoList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ToDoList() {
+    return (
+        <main>
+            <input type="checkbox" />
+            <span>Buy bread</span>
+            <br />
+
+            <input type="checkbox" />
+            <span>Clean car</span>
+            <br />
+
+            <input type="checkbox" />
+            <span>Feed fish</span>
+            <br />
+
+            <input type="checkbox" />
+            <span>Wash wellies</span>
+            <br />
+        </main>
+    )
+}
+
+export default ToDoList
---
+++ to_do_list/src/myComponents/Head/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+    return (
+        <header>
+            <h1>To Do List</h1>
+        </header>
+    )
+}
+
+export default Title
